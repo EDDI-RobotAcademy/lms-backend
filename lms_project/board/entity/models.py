f4c7ca16b07a289d4a0240bfe5c98a0a14a41317
Meue-L <meue7785@gmail.com>
2024-07-16 17:35:36 +0900

    feat: [MinJaeLee/Meue] Mock Test 생성 및 환경 파일 생성 [SKAIPx]

--- lms_project/board/entity/models.py
+++ lms_project/board/entity/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+
 class Board(models.Model):
-    id = models.AutoField(primary_key=True)
+    boardId = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128, null=False)
+    content = models.TextField()
 
-    def __str__(self):
-        return f"Cart -> id: {self.id}"
 
-    class Meta:
-        db_table = 'board'
+def __str__(self):
+    return f"Cart -> id: {self.id}"
+
+
+class Meta:
+    db_table = 'board'
