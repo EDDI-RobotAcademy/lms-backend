533f233dffcaa7dc9739c665a93e552cf12cb0af
lleehs <lhs3789@gmail.com>
2024-08-30 11:11:35 +0900

    Chore: [이현석] AccountAttendanceCheck 테이블 내용 수정[LMSD-18]
    Chore: [이현석] AccountAttendanceCheck 테이블 내용 수정[LMSD-18]

--- lms_project/account/entity/account_attendance_check.py
+++ lms_project/account/entity/account_attendance_check.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class AccountAttendanceCheck(models.Model):
     Attendance_cherry = models.IntegerField(default=0)
-    Attendance_date = models.IntegerField(default=0)
+    Monthly_attendance_flag = models.BooleanField(default=False)
+
+
 
     class Meta:
         db_table = 'account_attendance_check'
