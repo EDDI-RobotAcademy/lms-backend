4754ab63e4bad412fde98b69e4163bf371a27f8f
Meue-L <meue7785@gmail.com>
2024-09-03 11:25:11 +0900

    feat: [MinJaeLee/Meue] 출석체크시 출석체크 체리 추가 및 DB 업데이트 기능 구현 [LMSB-27]

--- lms_project/google_oauth/controller/views.py
+++ lms_project/google_oauth/controller/views.py
@@ -256,22 +256,26 @@ class GoogleOauthView(viewsets.ViewSet):
 
     def addAttendanceCherry(self, request):
         try:
-            user_token = request.data.get('userToken')
+            print(request)
+            user_token = request.data.get('usertoken')
             print("user_token 출력",user_token)
             accountInfo = self.redisService.getValueByKey(user_token)
-            print("어카운트 인포", accountInfo)
+            print("이전 어카운트 인포", accountInfo)
             account_id = accountInfo['account_id']
 
             attendanceCherry = self.accountService.findAttendance_CherryByAccountId(account_id)
+            attendanceCherry += 50
+            print("1번동작 성공: 현재 출석 체리 가져오기 및 출석으로 인한 체리 추가")
 
-            new_attendanceCherry = attendanceCherry + 50
+            self.accountService.updateAttendanceCherry(account_id, attendanceCherry)
+            print('2번동작 성공: sql에 변화된 체리 저장하기.')
 
-            self.accountService.updateAttendanceCherry(account_id, new_attendanceCherry)
-
-            accountInfo['attendance_cherry'] = new_attendanceCherry
+            accountInfo['attendance_cherry'] = attendanceCherry
             self.redisService.update_attendance_cherry_count(user_token, accountInfo)
+            print('마지막 동작 성공: 레디스에 변화된 체리 저장하기.')
+            print(f"전체 구동 성공: 이후 사용자의 정보->{accountInfo}")
 
-            return Response({'attendence_cherry': new_attendanceCherry}, status=status.HTTP_200_OK)
+            return Response({'attendence_cherry': attendanceCherry}, status=status.HTTP_200_OK)
         except Exception as e:
             print(f"Error using ticket: {e}")
             return False, str(e)
