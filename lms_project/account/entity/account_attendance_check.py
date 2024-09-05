581cee71fc164313f4cd8f7148d53bfc3fdd9d71
Meue-L <meue7785@gmail.com>
2024-09-05 12:40:41 +0900

    refactor: [MinJaeLee/Meue] 사용하지 않는 컬럼 삭제 및 관련 함수 내용 삭제 [LMSB-32]

--- lms_project/account/entity/account_attendance_check.py
+++ lms_project/account/entity/account_attendance_check.py
@@ -4,7 +4,6 @@ from django.db import models
 class AccountAttendanceCheck(models.Model):
     Attendance_cherry = models.IntegerField(default=0)
     Attendance_monthly_check = models.IntegerField(default=0)
-    Attendance_last_login_month = models.IntegerField(default=0)
 
     class Meta:
         db_table = 'account_attendance_check'
